0764ac1f3c72ee18d7367d8a4e170a596f1eb585
RodrigoPratas <rodrigompratas2002@gmail.com>
2023-02-19 21:43:58 +0000

    shell

--- main.c
+++ main.c
@@ -3,3 +3,14 @@
 //
 //
 
+int main(int argc, char **argv)
+{
+    // Load config files, if any.
+
+    // Run command loop.
+    lsh_loop();
+
+    // Perform any shutdown/cleanup.
+
+    return EXIT_SUCCESS;
+}
